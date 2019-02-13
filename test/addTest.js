96112c7605bbe7723e3a42435bbfb23d05d3f648
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-02-13 16:44:54 +0530

    Initail commit added git ignore

---
+++ test/addTest.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+const {add}   = require("../add");
+describe("add", function() {
+  it("should return the add of two number", function() {
+    assert.equal(add(7, 7), 14);
+  });
+});
