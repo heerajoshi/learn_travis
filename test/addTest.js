60a2f8e0e83e7cd781d34c1f1b8c01eaa739079e
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2019-02-16 10:33:52 +0530

    added one more test case for add function

--- test/addTest.js
+++ test/addTest.js
@@ -4,4 +4,7 @@ describe("add", function() {
   it("should return the add of two number", function() {
     assert.equal(add(7, 7), 14);
   });
+  it("should return the add of two negative number", function() {
+    assert.equal(add(-7, -7), -14);
+  });
 });
